6c401d250c45accbbfd59d02e648a85ccbe3ee20
Nathan LaFreniere <nlf@andyet.net>
2012-06-12 15:02:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+precommit-hook
+==============
+
+Usage:
+    npm install precommit-hook
+
+
+Everything else is automatic! The npm install script will create the hook, and place a .jshintrc and .jshintignore file if they don't exist. To update, just install again. Only the hook itself will be overwritten.
---
+++ install.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+    path = require('path');
+
+var projectPath = path.resolve(__dirname, '../../'),
+    filePath = path.join(__dirname, 'files');
+
+console.log(__dirname);
+console.log(projectPath);
+path.exists(path.join(projectPath, '.git'), function (exists) {
+    if (exists) {
+        console.log('Found .git directory, adding pre-commit hook');
+        fs.createReadStream(path.join(filePath, 'pre-commit')).pipe(fs.createWriteStream(path.join(projectPath, '.git/hooks/pre-commit')));
+        fs.chmodSync(path.join(projectPath, '.git/hooks/pre-commit'), '755');
+    }
+    path.exists(path.join(projectPath, '.jshintignore'), function (exist) {
+        if (!exist) {
+            console.log('Did not find a .jshintignore, creating one');
+            fs.createReadStream(path.join(filePath, 'jshintignore')).pipe(fs.createWriteStream(path.join(projectPath, '.jshintignore')));
+        }
+        path.exists(path.join(projectPath, '.jshintrc'), function (exis) {
+            if (!exis) {
+                console.log('Did not find a .jshintrc, creating one');
+                fs.createReadStream(path.join(filePath, 'jshintrc')).pipe(fs.createWriteStream(path.join(projectPath, '.jshintrc')));
+            }
+        });
+    });
+});
+
