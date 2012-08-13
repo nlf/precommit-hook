b9af10be8740496b4ef6dea87f3dd8f8fe089787
Nathan LaFreniere <quitlahok@gmail.com>
2012-08-13 13:37:17 -0700

    converted to all synchronous functions to help eliminate race conditions

--- install.js
+++ install.js
@@ -1,33 +1,30 @@
 var fs = require('fs'),
     path = require('path');
 
-var fExists = fs.exists || path.exists;
+var existsSync = fs.existsSync || path.existsSync;
 
 var projectPath = path.resolve(__dirname, '../../'),
-    filePath = path.join(__dirname, 'files');
+    filePath = path.join(__dirname, 'files'),
+    pcPath = path.join(projectPath, '.git', 'hooks', 'pre-commit'),
+    jsiPath = path.join(projectPath, '.jshintignore'),
+    jsrcPath = path.join(projectPath, '.jshintrc');
 
-fExists(path.join(projectPath, '.git'), function (exists) {
-    if (exists) {
-        fExists(path.join(projectPath, '.git/hooks/pre-commit'), function (hookExists) {
-            if (hookExists) {
-                fs.unlinkSync(path.join(projectPath, '.git/hooks/pre-commit'));
-            }
-            console.log('Found .git directory, adding pre-commit hook');
-            fs.createReadStream(path.join(filePath, 'pre-commit')).pipe(fs.createWriteStream(path.join(projectPath, '.git/hooks/pre-commit')));
-            fs.chmodSync(path.join(projectPath, '.git/hooks/pre-commit'), '755');
-        });
-    }
-    fExists(path.join(projectPath, '.jshintignore'), function (exist) {
-        if (!exist) {
-            console.log('Did not find a .jshintignore, creating one');
-            fs.createReadStream(path.join(filePath, 'jshintignore')).pipe(fs.createWriteStream(path.join(projectPath, '.jshintignore')));
-        }
-        fExists(path.join(projectPath, '.jshintrc'), function (exis) {
-            if (!exis) {
-                console.log('Did not find a .jshintrc, creating one');
-                fs.createReadStream(path.join(filePath, 'jshintrc')).pipe(fs.createWriteStream(path.join(projectPath, '.jshintrc')));
-            }
-        });
-    });
-});
+if (existsSync(path.join(projectPath, '.git'))) {
+    if (existsSync(pcPath)) fs.unlinkSync(pcPath);
+    console.log('Found .git directory, adding pre-commit hook');
+    var pcHook = fs.readFileSync(path.join(filePath, 'pre-commit'));
+    fs.writeFileSync(pcPath, pcHook);
+    fs.chmodSync(pcPath, '755');
+}
 
+if (!existsSync(jsiPath)) {
+    console.log('Did not find a .jshintignore, creating one');
+    var jsiFile = fs.readFileSync(path.join(filePath, 'jshintignore'));
+    fs.writeFileSync(jsiPath, jsiFile);
+}
+
+if (!existsSync(jsrcPath)) {
+    console.log('Did not find a .jshintrc, creating one');
+    var jsrcFile = fs.readFileSync(path.join(filePath, 'jshintrc'));
+    fs.writeFileSync(jsrcPath, jsrcFile);
+}
